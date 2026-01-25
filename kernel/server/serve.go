5f92afd5dc863ff868fa811ceb701a683875b125
Davide Garberi <dade.garberi@gmail.com>
2026-01-25 15:11:08 +0100

    kernel: server: Use https for fixed port proxy when needed

--- kernel/server/serve.go
+++ kernel/server/serve.go
@@ -229,7 +229,7 @@ func Serve(fastMode bool) {
 
 	go func() {
 		time.Sleep(1 * time.Second)
-		go proxy.InitFixedPortService(host)
+		go proxy.InitFixedPortService(host, useTLS, certPath, keyPath)
 		go proxy.InitPublishService()
 		// 反代服务器启动失败不影响核心服务器启动
 	}()
